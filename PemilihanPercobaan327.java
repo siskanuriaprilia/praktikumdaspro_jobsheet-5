19601c63f88e641f757c042ad1362c2f40aa7032
siskanuriaprilia <aprilias878@gmail.com>
2023-09-28 15:06:26 +0700

    PemilihanPercobaan327_Siska Nuri Aprilia

--- PemilihanPercobaan327.java
+++ PemilihanPercobaan327.java
@@ -19,7 +19,7 @@ public class PemilihanPercobaan327 {
         case '+':
             hasil = angka1 + angka2;
             System.out.println(angka1 + " + " + angka2 + "=" + hasil);
-            break;
+        
         case '-':
             hasil= angka1 - angka2;
             System.out.println(angka1 + " - " + angka2 + "=" + hasil);
