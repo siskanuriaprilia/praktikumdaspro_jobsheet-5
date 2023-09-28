e63f5f145700d07cf293c9ca54ff6b6eef282492
siskanuriaprilia <aprilias878@gmail.com>
2023-09-28 14:44:27 +0700

    Pemilihanpercobaan227_Siska Nuri Aprlia2

--- PemilihanPercobaan227.java
+++ PemilihanPercobaan227.java
@@ -16,9 +16,34 @@ public class PemilihanPercobaan227 {
 
     float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
+    if (80 < total && total <= 100){
+      System.out.println("Nilai Huruf Anda = A \nNilai Setara Anda = 4 \nKualifikasi = Sangat Baik");
+    }
+    else if (73 < total && total <= 80){
+      System.out.println("Nilai Huruf Anda = B+ \nNilai Setara Anda = 3,5 \nKualifikasi = Lebih dari Baik");
+    }
+    else if (65 < total && total <= 73){
+      System.out.println("Nilai Huruf Anda = B \nNilai Setara Anda = 3 \nKualifikasi = Baik");
+    }
+    else if (60 < total && total <= 65){
+      System.out.println("Nilai Huruf Anda = C+ \nNilai Setara Anda = 2,5 \nKualifikasi = Lebih dari Cukup");
+    }
+    else if (50 < total && total <= 60){
+      System.out.println("Nilai Huruf Anda = C \nNilai Setara Anda = 2 \nKualifikasi = Cukup");
+    }
+    else if (39 < total && total <= 50){
+      System.out.println("Nilai Huruf Anda = D \nNilai Setara Anda = 1 \nKualifikasi = Kurang ");
+    }
+    else if (0 < total && total <= 39){
+      System.out.println("Nilai Huruf Anda = E \nNilai Setara Anda =0 \nKualifikasi = Gagal");
+    }
+    else {
+      System.out.println("Terjadi Kesalahan");
+    }
+
     String message = + total <65 ? "Remidi" : "Tidak remidi";
 
-    System.out.println("Nilai akhir = " + total + "sehingga" + message );
+    System.out.println("Nilai akhir = " + total + " sehingga " + message );
 
    } 
 }
