278389c06f95e881adf29941cfc00c1357e5cbb2
siskanuriaprilia <aprilias878@gmail.com>
2023-09-28 14:01:28 +0700

    PemilihanPercobaan127_Siska Nuri Aprilia

--- PemilihanPercobaan127.java
+++ PemilihanPercobaan127.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan127 {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner input27 = new Scanner(System.in);
 
         System.out.println("Masukkan angka:  ");
         int angka = input27.nextInt();
 
-        if (angka % 2 == 0)
-            System .out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka " +angka+" bilangan ganjil ");
+        String hasil;
+
+        hasil = (angka<=-1) ? " Bilangan negatif " : "Bilangan positif ";
+        System.out.println(angka + " adalah " + hasil);
               
     }
 }
---
+++ PemilihanPercobaan227.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan227 {
+   public static void main(String[] args ){
+
+    Scanner input27 = new Scanner (System.in);
+
+    System.out.print(" Nilai uas     : ");
+    float uas = input27.nextFloat();
+    System.out.print (" Nilai uts     : ");
+    float uts = input27.nextFloat();
+    System.out.print(" Nilai kuis    : ");
+    float kuis = input27.nextFloat();
+    System.out.print(" Nilai tugas   : ");
+    float tugas = input27.nextFloat();
+
+    float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+    String message = + total <65 ? "Remidi" : "Tidak remidi";
+
+    System.out.println("Nilai akhir = " + total + "sehingga" + message );
+
+   } 
+}
